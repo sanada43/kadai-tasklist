d0ff54001d6bab2e7322e63b2c71fd0fe8141eb7
sanada43 <sakurada09415@gmail.com>
2019-05-15 08:11:38 +0000

    add column

---
+++ db/migrate/20190515080302_rename_titre_column_to_books.rb
@@ -0,0 +1,5 @@
+class RenameTitreColumnToBooks < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :tasks, :title, :status
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_014555) do
+ActiveRecord::Schema.define(version: 2019_05_15_080302) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
